b7ecffe7bb15bb77e94e1534b8b2975e43b79f57
14518457 <kyleb.byrne@gmail.com>
2017-02-07 18:46:35 +0000

    Revert "Before encapsulation"
    This reverts commit dd25901c2b80593d58c73c5b786ba45927dd13ef.

--- COMP30050/PokerGame/src/poker/PlayingCard.java
+++ COMP30050/PokerGame/src/poker/PlayingCard.java
@@ -6,20 +6,21 @@ public class PlayingCard {
 	private int faceValue;
 	private int gameValue;
 
-	static public final String FACES[] = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" }; // array of all card faces
+	static public final String FACES[] = { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
 	static public final char HEARTS = 'H';
 	static public final char DIAMONDS = 'D';
 	static public final char CLUBS = 'C';
 	static public final char SPADES = 'S';
 
-	public PlayingCard(String cardFace, char cardSuit, int value) {
-		//Constructor only takes 3 values as it only needs 3. Face value and game value get set of of 'value'
+	public PlayingCard(String cardFace, char cardSuit, int value) // Constructor
+	{
 		suit = cardSuit;
 		face = cardFace;
 		faceValue = value;
-		if (face == "A") {
+		if (face == "A") // set gameValue for ace
+		{
 			gameValue = 14;
-		} else {
+		} else {		// set all other values
 			gameValue = value;
 		}
 	}
