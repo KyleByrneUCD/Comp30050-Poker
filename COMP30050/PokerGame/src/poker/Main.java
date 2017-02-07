0dbe5f17c5fb841586b0e45be14e0433c3d98655
14518457 <kyleb.byrne@gmail.com>
2017-02-07 19:53:18 +0000

    Assignment 2 Complete

--- COMP30050/PokerGame/src/poker/Main.java
+++ COMP30050/PokerGame/src/poker/Main.java
@@ -6,27 +6,28 @@ public class Main {
 
 	public static void main(String[] args) {
 		ArrayList<PlayingCard> hand = new ArrayList<PlayingCard>();
-		DeckOfCards deck = new DeckOfCards();
-		System.out.println(deck.deck);
-		PlayingCard card = deck.dealNext();
-		System.out.println(card.toString());
-		deck.returnCard(card);
-		System.out.println(deck.deck);
-		deck.shuffle();
+		DeckOfCards deck = new DeckOfCards();	// Create deck of cards
+		System.out.println(deck.getDeck());		// Print Deck
+		PlayingCard card = deck.dealNext();		// Deal a card
+		System.out.println(card.toString());	// Print the dealt card
+		System.out.println(deck.getDeck());		// Print Deck
+		deck.returnCard(card);					// Return the card to the deck
+		System.out.println(deck.getDeck());		// Print Deck
+		deck.shuffle();							// Shuffle the deck
 		for(int i = 0; i < 5; i++){
-			hand.add(deck.dealNext());
+			hand.add(deck.dealNext());			// Deal 5 cards
 		}
-		System.out.println(deck.deck);
+		System.out.println(deck.getDeck());		// Print Deck
 		for(int i = 0; i < 5; i++){
-			deck.returnCard(hand.get(i));
+			deck.returnCard(hand.get(i));		// Return the 5 cards
 		}
-		System.out.println(deck.deck);
+		System.out.println(deck.getDeck());		// Print Deck
 		for(int i = 0; i < 55; i++){
-			hand.add(deck.dealNext());
+			hand.add(deck.dealNext());			// Deal more cards than are available
 		}
-		System.out.println(deck.deck);
-		deck.reset();
-		System.out.println(deck.deck);
+		System.out.println(deck.getDeck());		// Print Deck
+		deck.reset();							// Reset Deck
+		System.out.println(deck.getDeck());		// Print Deck
 		
 		// Print out all cards 
 	}
